7daca3e7ced4232d0205cf1b56875279b0d9df02
EringiShimeji <eringi.shimeji.5296@gmail.com>
2024-01-07 13:35:14 +0900

    ex05

---
+++ module_01/ex05/Harl.cpp
@@ -0,0 +1,45 @@
+#include "Harl.hpp"
+#include <iostream>
+
+void Harl::complain(std::string level) {
+	const char *levels[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	void (Harl::*fns[])() = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+
+	for (int i = 0; i < sizeof(levels) / sizeof(char *); i++) {
+		if (level == levels[i]) {
+			(this->*(fns[i]))();
+			return;
+		}
+	}
+}
+
+void Harl::debug() {
+	std::cout
+		<< "\033[48;2;107;135;88m D \033[0m "
+		<< "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger."
+		<< " I really do!"
+		<< std::endl;
+}
+
+void Harl::info() {
+	std::cout
+		<< "\033[48;2;49;92;118m I \033[0m "
+		<< "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger!"
+		<< " If you did, I wouldn’t be asking for more!"
+		<< std::endl;
+}
+
+void Harl::warning() {
+	std::cout
+		<< "\033[48;2;187;181;42m \033[30mW \033[0m "
+		<< "I think I deserve to have some extra bacon for free."
+		<< " I’ve been coming for years whereas you started working here since last month."
+		<< std::endl;
+}
+
+void Harl::error() {
+	std::cout
+		<< "\033[48;2;207;90;84m \033[30mE \033[0m "
+		<< "This is unacceptable! I want to speak to the manager now."
+		<< std::endl;
+}
