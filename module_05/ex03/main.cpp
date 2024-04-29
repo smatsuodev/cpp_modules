831a3b6843b4bcf773f81ad7b822739c6d94779c
EringiShimeji <eringi.shimeji.5296@gmail.com>
2024-04-29 22:29:21 +0900

    add bad_alloc of 05-03

--- module_05/ex03/main.cpp
+++ module_05/ex03/main.cpp
@@ -4,40 +4,43 @@
 
 int main() {
 	Intern intern;
+	try {
+		{
+			std::cout << "[Test 1] Make ShrubberyCreationForm" << std::endl;
+			AForm *form = intern.makeForm("shrubbery creation", "target");
+			assert(form->getName() == "ShrubberyCreationForm");
+			std::cout << *form << std::endl;
+			delete form;
+		}
 
-	{
-		std::cout << "[Test 1] Make ShrubberyCreationForm" << std::endl;
-		AForm *form = intern.makeForm("shrubbery creation", "target");
-		assert(form->getName() == "ShrubberyCreationForm");
-		std::cout << *form << std::endl;
-		delete form;
-	}
+		std::cout << "---------------------" << std::endl;
 
-	std::cout << "---------------------" << std::endl;
+		{
+			std::cout << "[Test 2] Make RobotomyRequestForm" << std::endl;
+			AForm *form = intern.makeForm("robotomy request", "target");
+			assert(form->getName() == "RobotomyRequestForm");
+			std::cout << *form << std::endl;
+			delete form;
+		}
 
-	{
-		std::cout << "[Test 2] Make RobotomyRequestForm" << std::endl;
-		AForm *form = intern.makeForm("robotomy request", "target");
-		assert(form->getName() == "RobotomyRequestForm");
-		std::cout << *form << std::endl;
-		delete form;
-	}
+		std::cout << "---------------------" << std::endl;
 
-	std::cout << "---------------------" << std::endl;
+		{
+			std::cout << "[Test 3] Make PresidentialPardonForm" << std::endl;
+			AForm *form = intern.makeForm("presidential pardon", "target");
+			assert(form->getName() == "PresidentialPardonForm");
+			std::cout << *form << std::endl;
+			delete form;
+		}
 
-	{
-		std::cout << "[Test 3] Make PresidentialPardonForm" << std::endl;
-		AForm *form = intern.makeForm("presidential pardon", "target");
-		assert(form->getName() == "PresidentialPardonForm");
-		std::cout << *form << std::endl;
-		delete form;
-	}
+		std::cout << "---------------------" << std::endl;
 
-	std::cout << "---------------------" << std::endl;
-
-	{
-		std::cout << "[Test 4] Form doesn't exist" << std::endl;
-		AForm *form = intern.makeForm("form doesn't exist", "target");
-		assert(form == NULL);
+		{
+			std::cout << "[Test 4] Form doesn't exist" << std::endl;
+			AForm *form = intern.makeForm("form doesn't exist", "target");
+			assert(form == NULL);
+		}
+	} catch (const std::bad_alloc &e) {
+		std::cout << e.what() << std::endl;
 	}
 }
