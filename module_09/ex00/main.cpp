686f7c5e27f977fed264541439734706b1704d2b
smatsuodev <git@smatsuo.dev>
2026-01-02 23:17:43 +0900

    fix main

--- module_09/ex00/main.cpp
+++ module_09/ex00/main.cpp
@@ -2,16 +2,21 @@
 #include <iostream>
 
 int main(int argc, char **argv) {
-    if (argc != 2) {
-        std::cerr << "Error: invalid number of arguments" << std::endl;
+    try {
+        if (argc != 2) {
+            std::cerr << "Error: invalid number of arguments" << std::endl;
+            return 1;
+        }
+
+        BitcoinExchange btc;
+
+        btc.loadDatabase("data.csv");
+
+        btc.processInput(argv[1]);
+    } catch (const std::exception& e) {
+        std::cerr << "Exception: " << e.what() << std::endl;
         return 1;
     }
-
-    BitcoinExchange btc;
-
-    btc.loadDatabase("data.csv");
-
-    btc.processInput(argv[1]);
 
     return 0;
 }
